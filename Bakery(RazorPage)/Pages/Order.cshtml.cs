7b5fc6811671d7ddd09d53bd63d5a9f02907063f
Tiago Goncalves <85870899+tiagogoncalvestg@users.noreply.github.com>
2022-08-30 10:43:55 -0300

    validations of scripts included

--- Bakery(RazorPage)/Pages/Order.cshtml.cs
+++ Bakery(RazorPage)/Pages/Order.cshtml.cs
@@ -2,6 +2,7 @@ using Bakery_RazorPage_.Data;
 using Bakery_RazorPage_.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bakery_RazorPage_.Pages
 {
@@ -15,9 +16,24 @@ namespace Bakery_RazorPage_.Pages
         [BindProperty(SupportsGet = true)]
         public int Id { get; set; }
         public Product Product { get; set; }
+        [BindProperty, EmailAddress, Required, Display(Name = "Your Email Address")]
+        public string OrderEmail { get; set; }
+        [BindProperty, Required(ErrorMessage = "Please supply a shipping address"), Display(Name = "Shipping Address")]
+        public string OrderShipping { get; set; }
+        [BindProperty, Display(Name = "Quantity")]
+        public int OrderQuantity { get; set; } = 1;
         public async Task OnGetAsync()
         {
             Product = await _context.Products.FindAsync(Id);
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            Product = await _context.Products.FindAsync(Id);
+            if (ModelState.IsValid)
+            {
+                return RedirectToPage("OrderSuccess");
+            }
+            return Page();
         }
     }
 }
---
+++ Bakery(RazorPage)/Pages/OrderSuccess.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bakery_RazorPage_.Pages
+{
+    public class OrderSuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
